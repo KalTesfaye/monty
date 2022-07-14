d15c7bbbbc4e87015bd44d12e19ad0e8bf669515
Kalkidan Bogale <kaltesfaye1990@gmail.com>
2022-07-14 14:40:30 -0700

    final 1

--- monty.h
+++ monty.h
@@ -1,20 +1,15 @@
-#ifndef __MONTY_H__
-#define __MONTY_H__
+#ifndef _MONTY_H_
+#define _MONTY_H_
 
 #include <stdlib.h>
 #include <stdio.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-#include <ctype.h>
-
-#include <stdbool.h>
 #include <string.h>
+#include <errno.h>
+#include <limits.h>
+#include <unistd.h>
 
-#define DELIMS "\n \r\t"
-#define  _POSIX_C_SOURCE 200809L
-
- /**
+int var_glob[2];
+/**
  * struct stack_s - doubly linked list representation of a stack (or queue)
  * @n: integer
  * @prev: points to the previous element of the stack (or queue)
@@ -43,15 +38,10 @@ typedef struct instruction_s
         char *opcode;
         void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
-extern stack_t **global_head;
 
-void global_free(void);
-
-void read_file(char *file, stack_t **stack);
-void parse_command(stack_t **stack, char *op, unsigned int line_num);
-
-void push(stack_t **stack, unsigned int line_number);
 void pall(stack_t **stack, unsigned int line_number);
+void push(stack_t **stack, unsigned int line_number);
+void functions_monty(stack_t **stack, char *command_f, unsigned int line_numb);
 void pint(stack_t **stack, unsigned int line_number);
 void pop(stack_t **stack, unsigned int line_number);
 void swap(stack_t **stack, unsigned int line_number);
@@ -61,9 +51,6 @@ void sub(stack_t **stack, unsigned int line_number);
 void _div(stack_t **stack, unsigned int line_number);
 void mul(stack_t **stack, unsigned int line_number);
 void mod(stack_t **stack, unsigned int line_number);
-void pchar(stack_t **stack, unsigned int line_number);
-void pstr(stack_t **stack, unsigned int line_number);
-void rotl(stack_t **stack, unsigned int line_number);
-void rotr(stack_t **stack, unsigned int line_number);
-
+void free_malloc(stack_t *head);
 #endif
+
