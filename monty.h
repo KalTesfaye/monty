caeb6f9469eef1983eb10b2cb370fdf1a1685026
Kalkidan Bogale <kaltesfaye1990@gmail.com>
2022-07-14 14:03:34 -0700

    push pall

--- monty.h
+++ monty.h
@@ -1,16 +1,20 @@
-#ifndef _MONTY_H_
-#define _MONTY_H_
+#ifndef __MONTY_H__
+#define __MONTY_H__
 
 #include <stdlib.h>
 #include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <ctype.h>
+
+#include <stdbool.h>
 #include <string.h>
-#include <errno.h>
-#include <limits.h>
-#include <unistd.h>
 
-int var_glob[2];
+#define DELIMS "\n \r\t"
+#define  _POSIX_C_SOURCE 200809L
 
-/**
+ /**
  * struct stack_s - doubly linked list representation of a stack (or queue)
  * @n: integer
  * @prev: points to the previous element of the stack (or queue)
@@ -39,6 +43,12 @@ typedef struct instruction_s
         char *opcode;
         void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
+extern stack_t **global_head;
+
+void global_free(void);
+
+void read_file(char *file, stack_t **stack);
+void parse_command(stack_t **stack, char *op, unsigned int line_num);
 
 void push(stack_t **stack, unsigned int line_number);
 void pall(stack_t **stack, unsigned int line_number);
@@ -47,5 +57,13 @@ void pop(stack_t **stack, unsigned int line_number);
 void swap(stack_t **stack, unsigned int line_number);
 void add(stack_t **stack, unsigned int line_number);
 void nop(stack_t **stack, unsigned int line_number);
+void sub(stack_t **stack, unsigned int line_number);
+void _div(stack_t **stack, unsigned int line_number);
+void mul(stack_t **stack, unsigned int line_number);
+void mod(stack_t **stack, unsigned int line_number);
+void pchar(stack_t **stack, unsigned int line_number);
+void pstr(stack_t **stack, unsigned int line_number);
+void rotl(stack_t **stack, unsigned int line_number);
+void rotr(stack_t **stack, unsigned int line_number);
 
 #endif
--- push.c
+++ push.c
@@ -9,28 +9,30 @@
  */
 void push(stack_t **stack, unsigned int line_number)
 {
-	stack_t *node = NULL;
-	stack_t *copy = *stack;
-	(void)line_number;
+	stack_t *node;
+	char *num;
 
-	if (stack == NULL)
+	num = strtok(NULL, DELIMS);
+	if (num == NULL)
 	{
-		fprintf(stderr, "L%d: usage: push integer\n", line_number);
-		var_glob[1] = 1;
-		return;
+		printf("L%u: usage: push integer\n", line_number);
+		exit(EXIT_FAILURE);
 	}
+
 	node = malloc(sizeof(stack_t));
 	if (node == NULL)
 	{
-		fprintf(stderr, "Error: malloc failed\n");
-		free(stack);
-		var_glob[1] = 1;
-		return;
+		printf("Error: malloc failed\n");
+		exit(EXIT_FAILURE);
 	}
+
+	node->n = atoi(num);
 	node->prev = NULL;
-	node->n = var_glob[0];
 	node->next = *stack;
-	if (*stack)
-		copy->prev = node;
+
+	if (*stack != NULL)
+		(*stack)->prev = node;
+
 	*stack = node;
 }
+
